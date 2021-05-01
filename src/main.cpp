40e4c955b7fd3cb710195e3361d5b53b05d32f18
Adrian Epifanio Rodriguez <alu0101158280@ull.edu.es>
2021-05-01 11:34:06 +0100

    Updated user manual

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@
 * @Author: Adrian Epifanio
 * @Date:   2021-04-21 12:55:55
 * @Last Modified by:   Adrian Epifanio
-* @Last Modified time: 2021-04-30 16:35:46
+* @Last Modified time: 2021-05-01 11:16:13
 */
 /*------------------  FUNCTIONS  -----------------*/
 
@@ -65,53 +65,18 @@ int main (int argc, char* argv[]) {
  * @brief      Prints help information about how to run and use the program.
  */
 void printHelp (void) {
-	std::cout << std::endl << "NAME" << std::endl;
-	std::cout << "\t textClassifier \t Generates vocabulary, corpus and classifies a text into different types of data." << std::endl;
-	
-	std::cout << std::endl << "SYNOPSIS" << std::endl;
-	std::cout << "\t ./textClassifier [-v | -co | -h] [arg ...]" << std::endl;
-	
-	std::cout << std::endl << "DESCRIPTION" << std::endl;
-	std::cout << "\t Generates vocabulary, corpus and classifies a text into different types of data." << std::endl << std::endl;
-	std::cout << "\t Runs the program with the needed flag for generating vocabulary (-v | --vocabulary), generating corpus (-co | --corpus), printin user manual (-h | --help)." << std::endl << std::endl;
-
-	std::cout << "\t Options:" << std::endl;
-	std::cout << "\t\t -v \t\t (--vocabulary) generates the vocabulary from the input file into the output file, excluding words in reserved words file." << std::endl;
-	std::cout << "\t\t -co\t\t (--corpus) generates one different corpus per each data type received as argument after the input file and the reserved wirds file." << std::endl;
-	std::cout << "\t\t -h \t\t (--help) prints the user manual for the program." << std::endl;
-
-	std::cout << "\t Exit Status:" << std::endl;
-	std::cout << "\t\t Returns success unless an error occurs" << std::endl << std::endl;
-
-	std::cout << "AUTHOR" << std::endl;
-	std::cout << "\t Written by Adrián Epifanio Rodríguez Hernández." << std::endl << std::endl;
-
-
-	std::cout << "REPORTING BUGS" << std::endl;
-	std::cout << "\t Report an issue at <https://github.com/AdrianEpi/Text-Classifier>" << std::endl << std::endl;
-
-	std::cout << "COPYRIGHT" << std::endl;
-	std::cout << "\t Copyright (c) 2021 Adrián Epifanio" << std::endl;
-
-
-    std::cout << "\tPermission is hereby granted, free of charge, to any person obtaining a copy" << std::endl;
-    std::cout << "\tof this software and associated documentation files (the \"Software\"), to deal" << std::endl;
-    std::cout << "\tin the Software without restriction, including without limitation the rights" << std::endl;
-    std::cout << "\tto use, copy, modify, merge, publish, distribute, sublicense, and/or sell" << std::endl;
-    std::cout << "\tcopies of the Software, and to permit persons to whom the Software is" << std::endl;
-    std::cout << "\tfurnished to do so, subject to the following conditions:" << std::endl << std::endl;
-
-    std::cout << "\tThe above copyright notice and this permission notice shall be included in all" << std::endl;
-    std::cout << "\tcopies or substantial portions of the Software." << std::endl << std::endl;
-
-    std::cout << "\tTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR" << std::endl;
-    std::cout << "\tIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY," << std::endl;
-    std::cout << "\tFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE" << std::endl;
-    std::cout << "\tAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER" << std::endl;
-    std::cout << "\tLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM," << std::endl;
-    std::cout << "\tOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE" << std::endl;
-    std::cout << "\tSOFTWARE." << std::endl << std::endl;	
-
+	const std::string MAN = "../include/man.txt";
+	std::ifstream file(MAN, std::ios::in);
+	if (file.fail()) {
+		std::cout << std::endl << "Error while opening manual file, make sure the file \"man.txt\" is placed on the include folder." << std::endl;
+		std::cout << "You can also read the instructions at <https://github.com/AdrianEpi/Text-Classifier> " << std::endl << std::endl;
+		exit(1);
+	} 
+	std::string reader;
+	while (!file.eof()) {
+		std::getline(file, reader);
+		std::cout << reader << std::endl;
+	}
 }
 
 /**
